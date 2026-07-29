943100f3c4d8eb2b68569ce2560fde1b087dc51c
talhashafi <talhashafi.ts@gmail.com>
2026-07-29 14:01:41 +0100

    creating multiple compartments and a parent child relationship with compartment00 and compartmentgroup

--- Stacks/Compartment/main.tf
+++ Stacks/Compartment/main.tf
@@ -1,6 +1,19 @@
-resource "oci_identity_compartment" "compartment01" {
+locals {
+  compartments = ["compartment01", "compartment02", "compartment03", "compartment04"]
+}
+
+resource "oci_identity_compartment" "compartment00" {
   name           = "testcompartment"
   description    = "compartment created by terraform as a test for learning"
   compartment_id = var.tenancy_ocid
   enable_delete  = false // true will cause this compartment to be deleted when running `terrafrom destroy`
+}
+
+resource "oci_identity_compartment" "compartments_group" {
+
+  for_each = toset(local.compartments)
+
+  name           = each.value
+  compartment_id = compartment00.id
+
 }
