7c5c1f99b3f9bed1b2cd68c12ffc6693ff2e4336
talhashafi <talhashafi.ts@gmail.com>
2026-07-31 11:38:20 +0100

    added var. to compartment reference

--- Stacks/IAM/main.tf
+++ Stacks/IAM/main.tf
@@ -10,7 +10,7 @@ resource "oci_identity_policy" "example_policy" {
   description    = "policy created by terraform"
   compartment_id = var.tenancy_ocid
   statements = [
-    "Allow group ${oci_identity_group.example_group.name} to read instances in compartment ${compute}",
-    "Allow group ${oci_identity_group.example_group.name} to inspect instances in compartment ${applications}",
+    "Allow group ${oci_identity_group.example_group.name} to read instances in compartment ${var.compute}",
+    "Allow group ${oci_identity_group.example_group.name} to inspect instances in compartment ${var.applications}",
   ]
 }
