f1ff658e96ffbe307096978db423a05088a1df1e
talhashafi <talhashafi.ts@gmail.com>
2026-07-31 11:40:24 +0100

    fixed variable reference error

--- Stacks/IAM/main.tf
+++ Stacks/IAM/main.tf
@@ -11,6 +11,6 @@ resource "oci_identity_policy" "example_policy" {
   compartment_id = var.tenancy_ocid
   statements = [
     "Allow group ${oci_identity_group.example_group.name} to read instances in compartment ${var.compute}",
-    "Allow group ${oci_identity_group.example_group.name} to inspect instances in compartment ${var.applications}",
+    "Allow group ${oci_identity_group.example_group.name} to inspect instances in compartment ${var.application}",
   ]
 }
