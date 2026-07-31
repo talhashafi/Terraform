0189639531479bff6ffd438f15862d42fe86a7e2
talhashafi <talhashafi.ts@gmail.com>
2026-07-31 11:22:41 +0100

    created group and policy

---
+++ Stacks/IAM/main.tf
@@ -0,0 +1,16 @@
+
+resource "oci_identity_group" "example_group" {
+  name           = "tf-example-group"
+  description    = "group created by terraform"
+  compartment_id = var.tenancy_ocid
+}
+
+resource "oci_identity_policy" "example_policy" {
+  name           = "tf-example-policy"
+  description    = "policy created by terraform"
+  compartment_id = var.tenancy_ocid
+  statements = [
+    "Allow group ${oci_identity_group.example_group.name} to read instances in compartment ${compute}",
+    "Allow group ${oci_identity_group.example_group.name} to inspect instances in compartment ${applications}",
+  ]
+}
