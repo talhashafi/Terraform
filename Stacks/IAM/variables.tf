a6d17f6a14d9e53f8a6ee3572c7c34d7d2e9d6e9
talhashafi <talhashafi.ts@gmail.com>
2026-07-31 11:22:26 +0100

    added compartment names in the variables file

---
+++ Stacks/IAM/variables.tf
@@ -0,0 +1,19 @@
+variable "tenancy_ocid" {   
+    type = string
+}
+
+variable "compute" {
+    default = compute
+}
+
+variable "application" {
+    default = application
+}
+
+variable "networks" {
+    default = networks
+}
+
+variable "security" {
+    default = security
+}
